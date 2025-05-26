79a73026ab7df6da075f95c43808b65a764b83fd
Артемий Беляков (Москва) <a.belyakov@cloud.gcm>
2025-05-26 17:47:23 +0300

    Написать тест приложения
    Необходимо протестить полное приложение
    	modified:   cmd/helpers.go
    	modified:   internal/confighandler/types.go
    	new file:   test/natsapi/sendrequest_test.go
    	modified:   version

---
+++ test/natsapi/sendrequest_test.go
@@ -0,0 +1,18 @@
+package natsapi_test
+
+import (
+	"testing"
+)
+
+/*
+здесь надо написать тест запроса получения
+подробной информации о сенсоре через NATS
+*/
+
+func TestGetSensorCommonInfo(t *testing.T) {
+	//инициализация с NATS
+
+	//запрос информации через NATS
+
+	// обработка ответа
+}
